0393b84cfd59c3f458f41744ef6bd7deca080a7f
whos-bhdn <anonimoos13@gmail.com>
2021-06-18 13:05:05 +0300

    one

---
+++ index.js
@@ -0,0 +1,43 @@
+// let bgColor = prompt('Enter the background color')
+// document.body.style.backgroundColor = bgColor;
+
+// -----------------------------------------------------------------------------
+
+// const font = prompt('Enter type of font (oblique, italic, normal)');
+// document.body.style.fontStyle = font;
+
+// -----------------------------------------------------------------------------
+
+// const centerAlign = prompt('Enter align-items value')
+// let header = document.querySelector('#head')
+// header.style.textAlign = centerAlign;
+
+// -----------------------------------------------------------------------------
+
+// let phrefs = prompt('Enter background color for href block');
+// let hrefBlock = document.body.children[1];
+// hrefBlock.style.backgroundColor = phrefs;
+
+// -----------------------------------------------------------------------------
+
+// let textColor = prompt('Enter color of the text');
+//       for(let i = 0; i < plinks.children.length; i++){
+//       plinks.children[i].style.color= textColor;
+//    }
+// -----------------------------------------------------------------------------
+
+// let divBg = prompt('Enter a DIV background color');
+// let divSz = prompt('Enter a DIV font size (with px)');
+// let divWdt = prompt('Enter a DIV width(with px)');
+// let divBlock = document.querySelector('.properties');
+// divBlock.style.backgroundColor = divBg;
+// divBlock.style.fontSize = divSz;
+// divBlock.style.width = divWdt;
+
+// -----------------------------------------------------------------------------
+
+// let typeMk = prompt('Enter a type of marker');
+// let blockUl = document.querySelector('.marker');
+// blockUl.style.listStyleType = typeMk;
+
+// -----------------------------------------------------------------------------
